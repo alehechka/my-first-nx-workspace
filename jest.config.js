8e70f2a555fc51ff7a7da23d7a9b367479f92233
alehechka <42357034+alehechka@users.noreply.github.com>
2020-10-15 15:00:07 -0500

    Initial Commit

---
+++ jest.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  projects: ['<rootDir>/apps/movie-client', '<rootDir>/libs/ui'],
+};
---
+++ libs/ui/src/lib/button/button.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './button.css';
+
+/* eslint-disable-next-line */
+export interface ButtonProps {}
+
+export const Button = (props: ButtonProps) => {
+  return (
+    <div>
+      <h1>Welcome to button!</h1>
+    </div>
+  );
+};
+
+export default Button;
